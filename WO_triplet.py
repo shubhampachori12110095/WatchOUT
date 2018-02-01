3a91efac367f08e29c126dfebb6ee8e37c4aac60
KyungMoon Lee <leekm8474@gmail.com>
2018-02-01 19:47:01 +0900

    import project

---
+++ WO_triplet.py
@@ -0,0 +1,105 @@
+import os
+import sys
+from keras.optimizers import Adam, RMSprop
+
+sys.path.append(os.path.join(os.getcwd(),"models"))
+from object_detection.utils import label_map_util
+#from object_detection.utils import visualization_utils as vis_util
+
+from helper import *
+
+
+def train():
+    PATH_TO_LABELS = os.path.join(os.getcwd(), 'watchout/data/deepfashion_label_map.pbtxt')
+    NUM_CLASSES = 50
+
+    label_map = label_map_util.load_labelmap(PATH_TO_LABELS)
+    categories = label_map_util.convert_label_map_to_categories(label_map, max_num_classes=NUM_CLASSES,
+                                                                use_display_name=True)
+    category_index = label_map_util.create_category_index(categories)
+
+    detection_graph = get_detector_graph()
+    triplet_graph = tf.Graph()
+    keras_weights_path = os.path.join(os.getcwd(), 'watchout/models/tripletnetwork/checkpoint')
+
+    with detection_graph.as_default():
+        with tf.Session(graph=detection_graph) as sess:
+            image_tensor = detection_graph.get_tensor_by_name('image_tensor:0')
+            detection_boxes = detection_graph.get_tensor_by_name('detection_boxes:0')
+            detection_scores = detection_graph.get_tensor_by_name('detection_scores:0')
+            detection_classes = detection_graph.get_tensor_by_name('detection_classes:0')
+            num_detections = detection_graph.get_tensor_by_name('num_detections:0')
+
+            # triplet_graph라는 전혀 다른 graph object를 할당한 session에서 실행
+            with tf.Session(graph=triplet_graph) as triplet_sess:
+                global_step = tf.get_variable('global_step', initializer=0, trainable=False)
+                increment_global_step = tf.assign(global_step, global_step + 1)
+
+                # 모델 resume 필요
+                triplet_model = get_triplet_model()
+                triplet_sess.run(tf.global_variables_initializer())
+
+                try:
+                    latest_weights, global_step_value = get_latest_weights_and_global_step(keras_weights_path)
+                    triplet_model.load_weights(latest_weights)
+                    triplet_sess.run(global_step.assign(global_step_value))
+                    print('latest weights loaded, global_step='+str(global_step_value))
+                except Exception as e:
+                    print('No weights file!')
+                    print(e)
+
+                triplet_model.compile(optimizer=Adam(lr=1e-7), loss=triplet_loss)
+
+                train_writer = tf.summary.FileWriter('./watchout/models/tripletnetwork/logs/train', triplet_sess.graph)
+                test_writer = tf.summary.FileWriter('./watchout/models/tripletnetwork/logs/test')
+
+                #while True:
+                for i in range(20):
+                    # anchor, positive, negative = get_train_data(_batch=40, d_sess=sess)
+                    anchor, positive, negative = get_train_data(_batch=4, d_sess=sess,
+                                                                d_tensors={'image_tensor': image_tensor,
+                                                                           'detection_boxes': detection_boxes,
+                                                                           'detection_scores': detection_scores,
+                                                                           'detection_classes': detection_classes,
+                                                                           'num_detections': num_detections})
+
+                    final_batch = anchor.shape[0]
+
+                    training_loss = triplet_model.train_on_batch(x=[anchor, positive, negative],
+                                                                 y=np.random.randint(2, size=(1, 2, final_batch)).T)
+                    triplet_sess.run(increment_global_step)
+                    print('train done')
+
+                    #if global_step.eval() % 200 == 0:
+                    if global_step.eval() % 1 == 0:
+                        train_summary = tf.Summary(value=[tf.Summary.Value(tag="train_loss",
+                                                                           simple_value=training_loss)])
+                        train_writer.add_summary(train_summary, global_step.eval())
+                        triplet_model.save_weights(os.path.join(os.getcwd(),
+                                                                'watchout/models/tripletnetwork/checkpoint/weights-' + str(
+                                                                    global_step.eval()) + '.hdf5'))
+                        print('train write done')
+
+                        #if global_step.eval() % 600 == 0:
+                        if global_step.eval() % 2 == 0:
+                            #_anchor, _positive, _negative = get_train_data(_batch=100, d_sess=sess)
+                            _anchor, _positive, _negative = get_train_data(_batch=4, d_sess=sess,
+                                                                           d_tensors={'image_tensor': image_tensor,
+                                                                                      'detection_boxes': detection_boxes,
+                                                                                      'detection_scores': detection_scores,
+                                                                                      'detection_classes': detection_classes,
+                                                                                      'num_detections': num_detections})
+                            final_test_batch = _anchor.shape[0]
+                            test_loss = triplet_model.test_on_batch(x=[_anchor, _positive, _negative],
+                                                                    y=np.random.randint(2, size=(1, 2, final_test_batch)).T)
+                            test_summary = tf.Summary(value=[tf.Summary.Value(tag="test_loss",
+                                                                              simple_value=test_loss)])
+                            test_writer.add_summary(test_summary, global_step.eval())
+                            print('test write done')
+def main(argv=None):
+    train()
+
+if __name__ == "__main__":
+    import warnings
+    warnings.filterwarnings('ignore')
+    tf.app.run()
---
+++ helper.py
@@ -0,0 +1,212 @@
+import numpy as np
+import os
+import sys
+import tensorflow as tf
+import pandas as pd
+import random
+from PIL import Image
+from keras.models import Model, Sequential
+from keras.layers import Dense, Flatten, Dropout, Input, Lambda
+from keras.applications.inception_v3 import InceptionV3
+from keras import backend as K
+
+sys.path.append(os.path.join(os.getcwd(),"models"))
+from object_detection.utils import label_map_util
+
+MODEL_NAME = 'faster_rcnn_resnet101_coco_11_06_2017'
+PATH_TO_CKPT = os.path.join(os.getcwd(), 'watchout/models/fasterRCNN/graph/frozen_inference_graph.pb')
+
+
+def load_image_into_numpy_array(image):
+	(im_width, im_height) = image.size
+	return np.array(image.getdata()).reshape(
+		(im_height, im_width, 3)).astype(np.uint8)
+
+
+def create_path(subpath, target):
+	return subpath + "/" + target
+
+
+def create_image_path(path):
+	return os.path.join(os.getcwd(), 'watchout/data/raw_deepfashion_dataset/Img/' + path)
+
+
+def random_sample(batch=1):
+	ann = Anno(os.path.join(os.getcwd(), 'watchout/data/raw_deepfashion_dataset/Anno'))
+	t_labels = []
+	t_paths = []
+	p_paths = []
+	n_labels = []
+	n_paths = []
+	for i in range(batch):
+
+		ran = random.randrange(0, len(ann))
+		target_path, target_label = (ann.loc[ran]['image_name'], ann.loc[ran]['category_label'])
+		positive_ran = random.choice(ann[ann['category_label'] == target_label].index.values)
+		positive_path = ann[ann['category_label'] == target_label].loc[positive_ran]['image_name']
+
+		while target_path == positive_path:
+			print('loop with "target_path == positive_path"')
+			positive_ran = random.choice(ann[ann['category_label'] == target_label].index.values)
+			positive_path = ann[ann['category_label'] == target_label].loc[positive_ran]['image_name']
+
+		negative_ran = random.choice(ann[ann['category_label'] != target_label].index.values)
+		negative_path = ann[ann['category_label'] != target_label].loc[negative_ran]['image_name']
+		negative_label = ann[ann['category_label'] != target_label].loc[negative_ran]['category_label']
+
+		t_labels.append(target_label)
+		t_paths.append(create_image_path(target_path))
+		p_paths.append(create_image_path(positive_path))
+		n_labels.append(negative_label)
+		n_paths.append(create_image_path(negative_path))
+
+	return (t_labels, t_paths, p_paths), (n_labels, n_paths)
+
+
+def Anno(path, head=False):
+	category_path = create_path(path, "list_category_img.txt")
+	category_data = pd.read_csv(category_path, sep=r"\s*", skiprows=[0], header=0)
+	if head:
+		return category_data.head(100)
+	return category_data
+
+
+def get_latest_weights_and_global_step(_path):
+	files = os.listdir(path=_path)
+	files.reverse()
+	return files[0], int(files[0].split('-')[1].split('.')[0])
+
+
+def promising_box_index(scores):
+	for idx, score in enumerate(scores):
+		if score < 0.8:  # 0.8 이상의 box만 리턴!
+			return idx
+	return len(scores)
+
+
+def promising_boxes(boxes, scores):
+	promising_idx = promising_box_index(scores)
+	return tf.slice(tf.squeeze(boxes), [0, 0], [promising_idx, 4])
+
+
+def images_from_paths(targets, positives, negatives):
+	assert len(targets) == len(positives) and len(positives) == len(negatives)
+	t_list = []
+	p_list = []
+	n_list = []
+	for t, p, n in zip(targets, positives, negatives):
+		t_single, p_single, n_single = image_from_path(t, p, n)
+		t_list.append(t_single)
+		p_list.append(p_single)
+		n_list.append(n_single)
+
+	return np.array(t_list), np.array(p_list), np.array(n_list)
+
+
+def image_from_path(target, positive, negative):
+	tmp_a = Image.open(target)
+	tmp_p = Image.open(positive)
+	tmp_n = Image.open(negative)
+
+	# arr = array(img, dtype="float32")
+
+	anchor = tmp_a.copy()
+	anchor = anchor.resize((299, 299), Image.ANTIALIAS)
+	anchor = np.array(anchor, dtype="float32")
+	pos = tmp_p.copy()
+	pos = pos.resize((299, 299), Image.ANTIALIAS)
+	pos = np.array(pos, dtype="float32")
+	neg = tmp_n.copy()
+	neg = neg.resize((299, 299), Image.ANTIALIAS)
+	neg = np.array(neg, dtype="float32")
+
+	tmp_a.close()
+	tmp_p.close()
+	tmp_n.close()
+
+	return anchor/255.0, pos/255.0, neg/255.0
+
+
+def euclidean_distance(vects):
+	anchor, target = vects
+	# return K.sqrt(K.maximum(K.sum(K.square(anchor - target), axis=1, keepdims=True), K.epsilon()))
+	return K.sum(K.square(anchor - target), axis=1, keepdims=True)
+
+
+def triplet_loss(y_true, y_pred):
+	margin = K.constant(0.2)
+	return K.mean(K.maximum(K.constant(0), K.square(y_pred[:, 0, 0]) - K.square(y_pred[:, 1, 0]) + margin))
+
+
+def get_triplet_model():
+	inceptionv3_input = Input(shape=(299, 299, 3))
+	inceptionv3_f = InceptionV3(include_top=False, weights='imagenet', input_tensor=inceptionv3_input)
+	net = inceptionv3_f.output
+	net = Flatten(name='flatten')(net)
+	net = Dense(1024, activation='relu', name='embedded')(net)
+
+	base_model = Model(inceptionv3_f.input, net, name='base_inceptionv3')
+
+	input_anchor = Input(shape=(299, 299, 3), name='input_anchor')
+	input_positive = Input(shape=(299, 299, 3), name='input_pos')
+	input_negative = Input(shape=(299, 299, 3), name='input_neg')
+
+	net_anchor = base_model(input_anchor)
+	net_positive = base_model(input_positive)
+	net_negative = base_model(input_negative)
+
+	d_positive = Lambda(euclidean_distance, name='d_pos')(
+		[net_anchor, net_positive])  # euclidean_distance((net_anchor, net_positive))
+	d_negative = Lambda(euclidean_distance, name='d_neg')(
+		[net_anchor, net_negative])  # euclidean_distance((net_anchor, net_negative))
+	d_stacked = Lambda(lambda vects: K.stack(vects, axis=1), name='d_stacked')([d_positive, d_negative])
+
+	triplet_model = Model([input_anchor, input_positive, input_negative], d_stacked, name='triplet_model')
+	return triplet_model
+
+
+def get_detector_graph():
+	detection_graph = tf.Graph()
+	with detection_graph.as_default():
+		od_graph_def = tf.GraphDef()
+		with tf.gfile.GFile(PATH_TO_CKPT, 'rb') as fid:
+			serialized_graph = fid.read()
+			od_graph_def.ParseFromString(serialized_graph)
+			tf.import_graph_def(od_graph_def, name='')
+	return detection_graph
+
+
+def get_train_data(_batch, d_sess,d_tensors):
+	(t_lbl, t_path, p_path), (n_lbl, n_path) = random_sample(_batch)
+
+	assert len(set([ x == y for (x,y) in zip(t_lbl, n_lbl)])) == 1 and list(set([ x == y for (x,y) in zip(t_lbl, n_lbl)]))[0] == False
+
+	anchor, positive, negative = images_from_paths(t_path, p_path, n_path)
+
+	for idx, single_batch in enumerate(t_path):
+
+		image = Image.open(single_batch)
+		image_np = load_image_into_numpy_array(image)
+		image_np_expanded = np.expand_dims(image_np, axis=0)
+		(_image_tensor, _boxes, scores, classes, num) = d_sess.run(
+			[d_tensors['image_tensor'], d_tensors['detection_boxes'], d_tensors['detection_scores'],
+			 d_tensors['detection_classes'], d_tensors['num_detections']],
+			feed_dict={d_tensors['image_tensor']: image_np_expanded})
+
+		target_boxes = promising_boxes(_boxes, scores[0])
+		len_target_boxes = int(target_boxes.shape[0])
+
+		cropped_images = []
+		for box in target_boxes.eval().tolist():
+			current_cropped_image = tf.image.crop_and_resize(image=image_np_expanded / 255.0,
+															 boxes=[box],
+															 box_ind=[0],
+															 crop_size=[299, 299])
+			cropped_images.append(current_cropped_image[0])
+			anchor = np.append(anchor, np.expand_dims(current_cropped_image[0].eval(), axis=0), axis=0)
+			positive = np.append(positive, np.expand_dims(positive[idx], axis=0), axis=0)
+			negative = np.append(negative, np.expand_dims(negative[idx], axis=0), axis=0)
+
+	assert anchor.shape[0] == positive.shape[0] and positive.shape[0] == negative.shape[0]
+
+	return anchor, positive, negative
